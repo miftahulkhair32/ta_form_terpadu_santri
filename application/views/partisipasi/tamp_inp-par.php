bbe88f14bf3617117fbb9945fadca802a8365fb5
miftahul khair <miftahul.khair.k.r@gmail.com>
2018-05-02 14:54:39 +0700

    perbaruan

--- application/views/partisipasi/tamp_inp-par.php
+++ application/views/partisipasi/tamp_inp-par.php
@@ -14,7 +14,7 @@
         </div>
         <div class="row">
             <div class="col-xs-12">
-            
+                <?= form_open('pelanggaran/proses_input') ?>
             </div>
         </div>
     </div>
