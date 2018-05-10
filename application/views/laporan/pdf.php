e677fe36969b96b6917558a7b76eec9ddc1739ce
miftahul khair <miftahul.khair.k.r@gmail.com>
2018-05-10 15:09:42 +0700

    sambungan pdf blom selesai

--- application/views/laporan/pdf.php
+++ application/views/laporan/pdf.php
@@ -33,14 +33,35 @@ $pdf->Ln();
 $pdf->SetFillColor(224,234,255);
 $pdf->SetTextColor(0);
 $pdf->SetFont('','',9);
-$pdf->Cell(8,10,'NO',1,0,'C',1);
-$pdf->Cell(40,10,'Nama',1,0,'C',1);
+$pdf->Cell(7,17.5,'NO',1,0,'C',1);
+$pdf->Cell(40,17.5,'Nama',1,0,'C',1);
 
 $pdf->SetFillColor(224,254,255);
-$pdf->Cell(60,3,'Karakter',1,1,'C',1);
-$pdf->SetFillColor(0,255,0);
-$pdf->MultiCell(19,5,'Partisipasi Program',1,'C',1,1,63,56,true);
-$pdf->MultiCell(14,5,'Jumlah Nilai',1,'C',1,1,81,56,true);
+$pdf->Cell(107,2,'Karakter',1,1,'C',1);
+$pdf->SetFillColor(124,244,119);
+$pdf->MultiCell(19,13,'Partisipasi Program',1,'C',1,1,62,56,true);
+$pdf->MultiCell(14,13,'Jumlah Nilai',1,'C',1,1,81,56,true);
+$pdf->SetFillColor(232,170,109);
+$pdf->MultiCell(21,13,'Target Pencapaian Diri',1,'C',1,1,95,56,true);
+$pdf->MultiCell(14,13,'Jumlah Nilai',1,'C',1,1,116,56,true);
+$pdf->SetFillColor(119,244,219);
+$pdf->MultiCell(18,13,'Nilai Observasi',1,'C',1,1,130,56,true);
+$pdf->SetFillColor(232,237,83);
+$pdf->MultiCell(10,13,'Total Nilai',1,'C',1,1,148,56,true);
+$pdf->MultiCell(11,13,'Huruf',1,'C',1,1,158,56,true);
+$pdf->SetFillColor(70,242,31);
+$pdf->MultiCell(17,13,'Halaman',1,'C',1,1,169,56,true);
+$pdf->SetFillColor(83,162,237);
+$pdf->MultiCell(51,2,'Tahfizh',1,'C',1,1,169,51.5,true);
+$pdf->MultiCell(14,13,'Jumlah Nilai',1,'C',1,1,185,56,true);
+$pdf->MultiCell(10,13,'Total Nilai',1,'C',1,1,199,56,true);
+$pdf->MultiCell(11,13,'Huruf',1,'C',1,1,209,56,true);
+$pdf->SetFillColor(239,79,79);
+$pdf->MultiCell(31,17.5,'Point Pelanggaran',1,'C',1,1,220,51.5,true);
+$pdf->Ln();
+
+$w = [40, 45, 50 ,55 , 60, 65 , 70];
+
 
 ob_clean();
 $pdf->Output('Laporan.pdf','I');
