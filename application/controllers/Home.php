94d12a9a9b5679cc4f51ca86e73d80d4f84b1acc
miftahul khair <miftahul.khair.k.r@gmail.com>
2018-05-11 20:45:39 +0700

    done

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,7 +6,7 @@ class Home extends CI_Controller {
     public function __construct(){
         parent::__construct();
         // letakkan magic kode disini
-        $this->load->model('m_user');
+        $this->load->model('m_home');
         $this->load->model('keamanan');
         $this->keamanan->cek_login();
     }
@@ -15,7 +15,11 @@ class Home extends CI_Controller {
         $isi = [
             'judul'     => 'Home',
             'subjudul'  => 'Dashboard',
-            'konten'    => 'konten'
+            'konten'    => 'konten',
+            'tgl'       => mediumdate_indo(date('d-m-Y')),
+            'san'       => $this->m_home->santri(),
+            'men'       => $this->m_home->menej(),
+            'total'     => $this->m_home->total()
         ];
         $this->load->view('standar',$isi);
     }
--- application/views/konten.php
+++ application/views/konten.php
@@ -21,3 +21,53 @@
         echo $info1;
     } ?>
 
+<div class="row">
+    <div class="space-6"></div>
+
+    <div class="col-xs-12 infobox-container">
+        <div class="infobox infobox-red">
+            <div class="infobox-icon">
+                <i class="ace-icon fa fa-calendar"></i>
+            </div>
+
+            <div class="infobox-data">
+                <span class="infobox-data-number"><?= $tgl ?></span>
+                <div class="infobox-content">Tanggal Sekarang</div>
+            </div>
+        </div>
+
+        <div class="infobox infobox-green">
+            <div class="infobox-icon">
+                <i class="ace-icon fa fa-users"></i>
+            </div>
+
+            <div class="infobox-data">
+                <span class="infobox-data-number"><?= $san ?></span>
+                <div class="infobox-content">Santri</div>
+            </div>
+        </div>
+
+        <div class="infobox infobox-blue">
+            <div class="infobox-icon">
+                <i class="ace-icon fa fa-users"></i>
+            </div>
+
+            <div class="infobox-data">
+                <span class="infobox-data-number"><?= $men ?></span>
+                <div class="infobox-content">Pengasuhan</div>
+            </div>
+        </div>
+
+        <div class="infobox infobox-pink">
+            <div class="infobox-icon">
+                <i class="ace-icon fa fa-users"></i>
+            </div>
+
+            <div class="infobox-data">
+                <span class="infobox-data-number"><?= $total ?></span>
+                <div class="infobox-content">Total User</div>
+            </div>
+        </div>
+    </div>
+</div>
+
